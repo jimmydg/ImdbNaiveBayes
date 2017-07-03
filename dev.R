e787bfa68084f84a82ddd47ad114f11e8cca8e9e
Jimmy de Graaf <degraafjimmy@gmail.com>
2017-07-03 14:40:47 +0200

    Removed minFreq function to meet higher accuracy, added other library

--- dev.R
+++ dev.R
@@ -1,99 +1,10 @@
-#https://rpubs.com/cen0te/naivebayes-sentimentpolarity
+blaat <- matrix(
+  c(seq(1, 10))
+)
+blaat
 
-install.packages("tm")
-install.packages("stringr")
-install.packages("caret")
-
-library(tm)  # text mining and document management
-library(stringr)  # character manipulation with regular expressions
-library(e1071)
-library(dplyr)
-library(caret)
-
-
-df <- read.csv("files/movie-pang02.csv", stringsAsFactors = FALSE)
-glimpse(df)
-
-#specify RNG seed
 set.seed(1)
+blaat <- blaat[sample(nrow(blaat)), ]
+blaat <- blaat[sample(nrow(blaat)), ]
 
-#randomize DF
-df <- df[sample(nrow(df)), ]
-
-glimpse(df)
-
-#convert to factors (0 and 1) not needed when train data already in 0 1
-df$class <- as.factor(df$class)
-
-#Initiate Corpus/Corpora collection from vector source
-corpus <- Corpus(VectorSource(df$text))
-
-#Clean the Corpus collection
-corpus.clean <- corpus %>%
-  tm_map(content_transformer(tolower)) %>% 
-  tm_map(removePunctuation) %>%
-  tm_map(removeNumbers) %>%
-  tm_map(removeWords, stopwords(kind="en")) %>%
-  tm_map(stripWhitespace)
-
-#Initiate a term-document matrix
-dtm <- DocumentTermMatrix(corpus.clean)
-
-inspect(dtm[40:50, 10:15])
-
-#initiate train and test data from datafram (df) data
-df.train <- df[1:1750,]
-df.test <- df[1751:2000,]
-
-#Initiate train and test data from documenttermmatrix (dtm) data
-dtm.train <- dtm[1:1750,]
-dtm.test <- dtm[1751:2000,]
-
-corpus.clean.train <- corpus.clean[1:1750]
-corpus.clean.test <- corpus.clean[1751:2000]
-
-dim(dtm.train)
-
-
-minfreq <- findFreqTerms(dtm.train, 10)
-length((minfreq))
-
-
-dtm.train.nb <- DocumentTermMatrix(corpus.clean.train, control=list(dictionary = minfreq))
-
-dim(dtm.train.nb)
-
-dtm.test.nb <- DocumentTermMatrix(corpus.clean.test, control=list(dictionary = minfreq))
-dim(dtm.test.nb)
-
-
-convert_count <- function(x) {
-  y <- ifelse(x > 0, 1,0)
-  y <- factor(y, levels=c(0,1), labels=c("No", "Yes"))
-  y
-}
-
-
-trainNB <- apply(dtm.train.nb, 2, convert_count)
-testNB <- apply(dtm.test.nb, 2, convert_count)
-
-
-system.time( classifier <- naiveBayes(trainNB, df.train$class, laplace = 1) )
-
-#saveRDS(classifier, "./classifier.rds")
-#classifierSaved<- readRDS("./classifier.rds")
-
-system.time( prediction <- predict(classifier, newdata=testNB) )
-length(prediction)
-
-
-saveRDS(prediction, "./prediction.rds")
-
-predictionSaved<- readRDS("./prediction.rds")
-
-
-table("Predictions"= prediction,  "Actual" = df.test$class )
-
-conf.mat <- confusionMatrix(prediction, df.test$class)
-
-conf.mat
+blaat
--- naiveBayes.R
+++ naiveBayes.R
@@ -20,10 +20,12 @@ set.seed(1)
 labeledTrainData <- labeledTrainData[sample(nrow(labeledTrainData)), ]
 labeledTrainData <- labeledTrainData[sample(nrow(labeledTrainData)), ]
 
-glimpse(df)
+#glimpse(labeledTrainData)
 
 #Initiate Corpus collection from source text
 corpus <- Corpus(VectorSource(labeledTrainData$review))
+
+#corpus[[143]]$content
 
 #Corpus clean function
 corpus.clean <- corpus %>%
@@ -33,49 +35,43 @@ corpus.clean <- corpus %>%
   tm_map(removeWords, stopwords(kind="en")) %>%
   tm_map(stripWhitespace)
 
-#Initiate a term-document matrix
+#Initiate a document-term matrix
 dtm <- DocumentTermMatrix(corpus.clean)
 
-#retrieve information from corpus
-inspect(dtm[40:50, 10:15])
+#retrieve information from DTM
+#inspect(dtm[40:50, 10:15])
 
-#initiate train and test data from datafram (df) data
-labeledTrainData.train <- labeledTrainData[1:1750,]
-labeledTrainData.test <- labeledTrainData[1751:2000,]
-
-#Initiate train and test data from documenttermmatrix (dtm) data
-dtm.train <- dtm[1:1750,]
-dtm.test <- dtm[1751:2000,]
-
-#clean the data
-corpus.clean.train <- corpus.clean[1:1750]
-corpus.clean.test <- corpus.clean[1751:2000]
-
-dim(dtm.train)
+#Train en testdata aanmaken van de originele data
+labeledTrainData.train <- labeledTrainData[1:1500, ]
+labeledTrainData.test <- labeledTrainData[1501:2000, ]
 
 
-minfreq <- findFreqTerms(dtm.train, 100)
-length((minfreq))
+#Train en testdata aanmaken van de dtm
+dtm.train <- dtm[1:1500, ]
+dtm.test <- dtm[1501:2000, ]
 
+#Data opschonen
+#minfreq niet gebruikt om hogere accuracy te behalen
+corpus.clean.train <- corpus.clean[1:1500]
+corpus.clean.test <- corpus.clean[1501:2000]
 
-dtm.train.nb <- DocumentTermMatrix(corpus.clean.train, control=list(dictionary = minfreq))
+dtm.train.nb <- DocumentTermMatrix(corpus.clean.train)
+dtm.test.nb <- DocumentTermMatrix(corpus.clean.test)
 
-dim(dtm.train.nb)
+#dim(dtm.train.nb)
+#dim(dtm.test.nb)
 
-dtm.test.nb <- DocumentTermMatrix(corpus.clean.test, control=list(dictionary = minfreq))
-dim(dtm.test.nb)
-
+#Function to replace 0 and 1's by boolean values (binarized Naive-bayes)
 convert_count <- function(x) {
   y <- ifelse(x > 0, 1,0)
   y <- factor(y, levels=c(0,1), labels=c("No", "Yes"))
   y
 }
 
-
 trainNB <- apply(dtm.train.nb, 2, convert_count)
 testNB <- apply(dtm.test.nb, 2, convert_count)
 
-#get time spent calculating and other info
+#initiate nb classifier
 system.time(
   classifier <- naiveBayes(trainNB, labeledTrainData.train$sentiment, laplace = 1)
   )
@@ -83,13 +79,16 @@ system.time(
 saveRDS(classifier, "./classifier.rds")
 #classifierSaved<- readRDS("./classifier.rds")
 
-system.time( prediction <- predict(classifier, newdata=testNB) )
-length(prediction)
+#initiatie prediction
+system.time(
+  prediction <- predict(classifier, newdata=as.matrix(testNB))
+  )
 
+#length(prediction)
 
 saveRDS(prediction, "./prediction.rds")
 
-predictionSaved<- readRDS("./prediction.rds")
+#predictionSaved<- readRDS("./prediction.rds")
 
 
 table("Predictions"= prediction,  "Actual" = labeledTrainData.test$sentiment )
@@ -97,3 +96,5 @@ table("Predictions"= prediction,  "Actual" = labeledTrainData.test$sentiment )
 conf.mat <- confusionMatrix(prediction, labeledTrainData.test$sentiment)
 
 conf.mat
+
+print(paste('accuracy: ', conf.mat$overall['Accuracy']))
